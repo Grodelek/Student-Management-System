8772c4a1b334f343b247242a8b0a2cab66b8fb4e
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-06-13 20:10:53 +0200

    Add files via upload

---
+++ person/src/main/java/student/person/UserController.java
@@ -0,0 +1,39 @@
+package student.person;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public String getUsers(Model model){
+        List<User> userList = userService.getAllUsers();
+        model.addAttribute("users", userList);
+        return "users";
+    }
+    @GetMapping("/register")
+    public String showUserForm(Model model){
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String addUser(@ModelAttribute User user){
+        userService.saveUser(user);
+        return "redirect:/users";
+    }
+
+
+
+}
---
+++ person/src/main/java/student/person/UserRepository.java
@@ -0,0 +1,6 @@
+package student.person;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
